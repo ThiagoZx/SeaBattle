e2161bf41bb24e4ae7158e806ee0aa0e84fa49c5
Thiago Torres <TheThiagoo0@gmail.com>
2015-11-25 10:36:23 -0200

    Grid on Canvas!
    Working properly.
    Remember to make it spawn on player Two too!
    After working on that, make it customizable... Allowing the player to
    choose from 5x5 to 10x10 should be ok.
    Start working on the server too, please.

--- BattleShip/Assets/Scripts/Spawner.cs
+++ BattleShip/Assets/Scripts/Spawner.cs
@@ -5,13 +5,19 @@ using System.Collections;
 public class Spawner : MonoBehaviour {
 
 	public Button button;
+	public int size;
 
 	void Start () {
+		spawnGrid ();
+	}
+
+	private void spawnGrid(){
 		for(int i = 0; i < 5; i++){	
 			for(int j = 0; j < 5; j++){
-				Instantiate(button, new Vector3(-300 + i * 30, 100 + j * 30, 0), Quaternion.identity);
+				Button btn;
+				btn = Instantiate(button, new Vector3(-300 + i * size, 50 - j * size, 0), Quaternion.identity) as Button;
+				btn.transform.SetParent(transform, false);
 			}
 		}
 	}
-
 }

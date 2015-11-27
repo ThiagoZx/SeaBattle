88328d09f88fbccdeb00ac43b64fc019b42ac566
Thiago Torres <TheThiagoo0@gmail.com>
2015-11-27 09:45:57 -0200

    The Power of the Gambiarra
    It doesn't matter as long as it works. Please, fix that later.. Use some
    kind of boolean or something.... Well, it's up to you.

---
+++ BattleShip/Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonBehaviour : MonoBehaviour, IPointerExitHandler
+{
+	public void OnPointerExit (PointerEventData eventData) 
+	{
+		gameObject.SetActive (false);
+		gameObject.SetActive (true);
+	}
+}
